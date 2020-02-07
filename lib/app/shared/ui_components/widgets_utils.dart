030b524d8d4aa07dab805d1517dbf8608efc0cf8
israelhudson <israelhudson@gmail.com>
2020-02-06 23:54:51 -0300

    removindo component usless

--- lib/app/shared/ui_components/widgets_utils.dart
+++ lib/app/shared/ui_components/widgets_utils.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:platform_alert_dialog/platform_alert_dialog.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
-alert(String text){
-  return Fluttertoast.showToast(
-      msg: "$text",
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIos: 1,
-      backgroundColor: Colors.red,
-      textColor: Colors.white,
-      fontSize: 16.0
-  );
-}
 
 alerDialog({String msg,BuildContext context, Function action}){
   return showDialog<void>(
